57c8b767ef3cc557ba069dfa4650aa76c1517f2a
MohammadDaadour <112201830+MohammadDaadour@users.noreply.github.com>
2023-03-09 19:12:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function collapse() {
+    btn1.addEventListener("click", () => {
+      p1.classList.toggle("inactive");
+    });
+
+    btn2.addEventListener("click", () => {
+      p2.classList.toggle("inactive");
+    });
+
+    btn3.addEventListener("click", () => {
+      p3.classList.toggle("inactive");
+    });
+
+    btn4.addEventListener("click", () => {
+      p4.classList.toggle("inactive");
+    });
+
+    btn5.addEventListener("click", () => {
+      p5.classList.toggle("inactive");
+    });
+
+    btn6.addEventListener("click", () => {
+      p6.classList.toggle("inactive");
+    });
+}
+
+collapse();
